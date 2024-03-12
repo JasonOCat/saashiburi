98971255d9bda218ac4b42080738379fe74146f6
JasonOCat <jason.catbagan@gmail.com>
2024-03-12 23:55:26 +0100

    Home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,1 +1,23 @@
-export default function HomePage() {}
+import Link from "next/link";
+
+export default function HomePage() {
+  return (
+    <div className="grid-halves h-screen-navbar">
+      <div className="bg-teal border-right">
+        <div className="column-padding">
+          <div className="tablet-centered">
+            <div className="content-grid home-hero">
+              <h1>
+                The most <br /> epic products.
+              </h1>
+              <p className="section-subtitle">All the most epic things on the internet, all in one place</p>
+              <Link href='products' className="large-button">
+                <div className="large-button-text">Explore Products</div>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
