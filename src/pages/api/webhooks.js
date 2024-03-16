892577ccdbb6299500dbde774d1f15b4c6bee424
JasonOCat <jason.catbagan@gmail.com>
2024-03-16 09:38:10 +0100

    Add stripe checkout update client webhook

--- src/pages/api/webhooks.js
+++ src/pages/api/webhooks.js
@@ -1,1 +1,31 @@
-export default async function handler(req, res) {}
+import getRawBody from "raw-body";
+import { stripe } from "src/pricing/utils/stripe";
+
+//Strip doc on webhooks : https://docs.stripe.com/webhooks
+// to get the buffer correctly using raw-body
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+export default async function handler(req, res) {
+  const signature = req.headers['stripe-signature'];
+  const signingSecret = process.env.STRIPE_SIGNING_SECRET;
+
+  let event;
+  try {
+    //to have a buffer
+    const rawBody = await getRawBody(req, { limit: "2mb"});
+    event = stripe.webhooks.constructEvent(rawBody, signature, signingSecret);
+
+  } catch (error) {
+    console.log("Webhook signature verification failed.")
+    return res.status(400).end();
+  }
+
+  console.log(event)
+
+  res.send({success: true})
+
+};
