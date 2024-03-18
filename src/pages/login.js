e5f989b280024496f06918f020c880b06b2bf9cb
JasonOCat <jason.catbagan@gmail.com>
2024-03-18 22:27:53 +0100

    Login page and form

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,5 +1,26 @@
+import Link from "next/link";
+import Logo from "src/core/components/Logo";
+import LoginForm from "src/login/components/LoginForm";
+import Image from "next/image";
+import login from '../../public/assets/login.png'
+
 export default function LoginPage() {
   return (
-    <div>hello world</div>
+    <div className="grid-halves h-screen">
+      <div className="border-right bg-offwhite">
+        <div className="column-padding">
+          <div className="tablet-centered">
+            <Link href="/" className="logo-container">
+              <Logo style={{ width: 150 }} />
+            </Link>
+            <LoginForm />
+          </div>
+        </div>
+      </div>
+      <div className="bg-navy border-right">
+        <Image src={login} alt="login" className="callout-image"/>
+
+      </div>
+    </div>
   )
 }
