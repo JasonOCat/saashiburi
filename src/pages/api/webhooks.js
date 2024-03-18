32bf2450281f46b34536507c083d69ae8acf7c6b
JasonOCat <jason.catbagan@gmail.com>
2024-03-18 05:44:16 +0100

    Create Profile in supabase, see tables.txt. Handle webhook stripe event customer updated and deleted

--- src/pages/api/webhooks.js
+++ src/pages/api/webhooks.js
@@ -1,5 +1,5 @@
 import getRawBody from "raw-body";
-import { stripe } from "src/pricing/utils/stripe";
+import {stripe} from "src/pricing/utils/stripe";
 
 //Strip doc on webhooks : https://docs.stripe.com/webhooks
 // to get the buffer correctly using raw-body
@@ -24,17 +24,32 @@ export default async function handler(req, res) {
     return res.status(400).end();
   }
 
+  // Types of Stripe Event https://docs.stripe.com/api/events/object
   try {
     switch (event.type) {
-      case "customer.subscription":
-        console.log("subscription");
+      case "customer.subscription.updated":
+        await updateSubscription(event);
+        break;
+
+      case "customer.subscription.deleted":
+        await deleteSubscription(event);
+        break;
+
     }
   } catch (error) {
     console.log(error.message);
-    res.send({succes: false});
+    res.send({success: false});
 
   }
 
   res.send({success: true})
 
 };
+
+async function updateSubscription(event){
+
+}
+
+async function deleteSubscription(event){
+
+}
