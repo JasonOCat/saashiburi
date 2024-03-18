ed3f661555f3e026c0837a6ddae6209eee901974
JasonOCat <jason.catbagan@gmail.com>
2024-03-18 22:27:39 +0100

    Add supabase session auth helpers

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,11 +1,23 @@
+import {SessionContextProvider} from "@supabase/auth-helpers-react";
+
 import "src/styles/globals.css";
 import AppLayout from "src/core/layouts/App";
+import {createBrowserSupabaseClient} from "@supabase/auth-helpers-nextjs";
+import {useState} from "react";
+
+
 
 export default function App({ Component, pageProps }) {
+  const [supabaseClient] = useState(() => createBrowserSupabaseClient())
   return (
-    <AppLayout>
-      <Component {...pageProps}/>
-    </AppLayout>
+    // doc supabase auth helpers : https://supabase.com/docs/guides/auth/auth-helpers/nextjs deprecated, use cookie now
+    <SessionContextProvider
+      supabaseClient={supabaseClient}
+      initialSession={pageProps.initialSession}>
+      <AppLayout>
+        <Component {...pageProps}/>
+      </AppLayout>
+    </SessionContextProvider>
   )
 
 }
