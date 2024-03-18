00b4285cfb04b4d676b6fd8b820b321d354efd89
JasonOCat <jason.catbagan@gmail.com>
2024-03-18 06:28:00 +0100

    After checkout, create customer if it exists, otherwise create a server-side auth client.

--- src/pages/api/webhooks.js
+++ src/pages/api/webhooks.js
@@ -1,5 +1,6 @@
 import getRawBody from "raw-body";
 import {stripe} from "src/pricing/utils/stripe";
+import {supabase} from "../../../supabase";
 
 //Strip doc on webhooks : https://docs.stripe.com/webhooks
 // to get the buffer correctly using raw-body
@@ -47,7 +48,46 @@ export default async function handler(req, res) {
 };
 
 async function updateSubscription(event){
+  const subscription = event.data.object
+  const stripe_customer_id = subscription.customer;
+  const subscription_status = subscription.status;
+  const price = subscription.items.data[0].price.id;
 
+  const { data: profile } = await supabase.from('profile')
+    .select('*')
+    .eq('stripe_customer_id', stripe_customer_id)
+    .single();
+
+  if (profile) {
+    // doc supabase to update profile https://supabase.com/dashboard/project/vtjajvdcirctoxlzhoei/api?resource=profile
+    const updatedSubscription = {
+      subscription_status,
+      price
+    }
+    await supabase
+      .from('profile')
+      .update(updatedSubscription)
+      .eq('stripe_customer_id', stripe_customer_id)
+  } else {
+    // doc stripe retrieve customer  https://docs.stripe.com/api/customers/retrieve
+    const customer = await stripe.customers.retrieve(stripe_customer_id);
+    const name = customer.name;
+    const email = customer.email
+    const newProfile = {
+      name,
+      email,
+      stripe_customer_id,
+      subscription_status,
+      price,
+    }
+
+    // doc supabase to create server-side auth client : https://supabase.com/docs/reference/javascript/admin-api/admin-api
+    await supabase.auth.admin.createUser({
+      email,
+      email_confirm: true,
+      user_metadata: newProfile
+    })
+  }
 }
 
 async function deleteSubscription(event){
