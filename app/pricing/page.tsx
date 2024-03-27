19c1c3a0e79b2aca69718b3c5dc1b5862f945cd0
JasonOCat <jason.catbagan@gmail.com>
2024-03-27 08:46:33 +0100

    Migrate pricing page, need to use server component when calling supabase client

---
+++ app/pricing/page.tsx
@@ -0,0 +1,39 @@
+import {stripe} from "@/src/pricing/utils/stripe";
+import Plans from "@/src/pricing/components/Plans";
+import Benefits from "@/src/pricing/components/Benefits";
+
+
+export default async function PricingPage() {
+  const {data: prices } = await stripe.prices.list()
+  const plans : any[] = []
+
+  for (const price of prices) {
+    const product = await stripe.products.retrieve(price.product as string)
+
+    if (!price?.unit_amount) {
+      throw new Error(`Unit amount is null or undefined for product ${product.name}`);
+    }
+
+    if (!price?.recurring?.interval) {
+      throw new Error(`Recurring interval is null or undefined for product ${product.name}`);
+    }
+
+    plans.push({
+      name: product.name,
+      id: price.id,
+      price: price.unit_amount / 100,
+      interval: price.recurring.interval
+    });
+
+
+  }
+  console.log(plans.length);
+
+
+  return (
+    <div className="grid-halves h-screen-navbar">
+      <Plans plans={plans} />
+      <Benefits />
+    </div>
+  )
+}
